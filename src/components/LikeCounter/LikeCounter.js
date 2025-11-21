e91a30aec12e0bbb48c229a4c561e9489e7a0a33
unknown <dcisek434@gmail.com>
2025-11-21 19:07:07 +0100

    like counter defines local state for number of likes

--- src/components/LikeCounter/LikeCounter.js
+++ src/components/LikeCounter/LikeCounter.js
@@ -1,10 +1,32 @@
 import './LikeCounter.css';
+import { useState } from 'react';
+
 
 function LikeCounter() {
+    const [likes, setLikes] = useState(0);
+    const handClick = () => {
+       setLikes(likes + 1);
+    };
+    const handClick1 = () => {
+        if (likes == 0) return;
+       setLikes(likes - 1);
+    };
+
+    const counterStyles = {
+        color: likes > 5 ? 'red' : 'black',
+        fontWeight: likes > 5 ? 'bold' : 'normal',
+        textAlign: "center"
+
+    };
+
     return (
         <div className='post'>
-            <p>Fajny post!! Liczba lajków X</p>
-            <button>Polub 👍</button>
+            <p style={counterStyles}>Liczba lajków {likes}</p>
+            <div className='actions'>
+                <button onClick={handClick} style={{ marginRight: "10px" }}>❤️ Polub</button>
+                <button onClick={handClick1}>👎 Usuń lajka</button>
+            </div>
+            
         </div>
     );
 };
